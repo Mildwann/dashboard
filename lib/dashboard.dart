2aeb9832986b4c0439962a9adb1f2b6f2c38f343
Mildwann <Mildwann46@gmail.com>
2024-06-06 13:18:08 +0700

    Structure UI

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,54 +1,354 @@
 import 'package:flutter/material.dart';
 
-class MyDashBord extends StatelessWidget {
-  const MyDashBord({super.key});
+class MyDashBord extends StatefulWidget {
+  const MyDashBord({Key? key}) : super(key: key);
+
+  @override
+  State<MyDashBord> createState() => _MyDashBordState();
+}
+
+class _MyDashBordState extends State<MyDashBord> {
+  TextEditingController userId1Controller = TextEditingController();
+  TextEditingController userId2Controller = TextEditingController();
+  TextEditingController userId3Controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    double Width = MediaQuery.of(context).size.width;
-    double Height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Column(
         children: [
           Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(30),
-              child: const Text(
-                "Dashboard",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              )),
-          Container(
             width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(0.3)),
-            margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
+            margin: const EdgeInsets.fromLTRB(30, 50, 30, 20),
+            child: const Text(
+              "Dashbord",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                    child: const Text(
-                  "Standard search",
-                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
-                )),
+                  width: double.infinity,
+                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  child: const Text("Standard search"),
+                ),
                 Container(
+                  margin: const EdgeInsets.all(20),
                   child: Row(
                     children: [
                       Container(
-                        child: const Column(
-                          children: [
-                            Text(
-                              "User ID",
-                              style: TextStyle(fontSize: 10),
-                            ),
-                          ],
+                        width: (width - 100) / 3,
+                        child: Container(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: TextField(
+                            controller: userId3Controller,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)),
+                                hintText: 'Search',
+                                suffixIcon: Icon(Icons.search)),
+                          ),
                         ),
-                      )
+                      ),
+                      Container(
+                        width: (width - 100) / 3,
+                        child: Container(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: TextField(
+                            controller: userId3Controller,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)),
+                                hintText: 'Search',
+                                suffixIcon: Icon(Icons.search)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: (width - 100) / 3,
+                        child: Container(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: TextField(
+                            controller: userId3Controller,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.teal)),
+                                hintText: 'Search',
+                                suffixIcon: Icon(Icons.search)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(right: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(onPressed: () {}, child: const Text("Clear")),
+                      TextButton(onPressed: () {}, child: const Text("Search")),
                     ],
                   ),
                 )
               ],
             ),
-          )
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(30, 50, 30, 20),
+            height: height * 0.3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    width: (width - 60) * 0.59,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Order Item"),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  color: Colors.black45,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  color: Colors.black45,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  color: Colors.black45,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  color: Colors.black45,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    width: (width - 60) * 0.39,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("data"),
+                        Expanded(
+                          child: Container(
+                            height: 50,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 50,
+                            color: Colors.deepPurple,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Box 1 with TabView
+          Container(
+            margin: const EdgeInsets.fromLTRB(30, 0, 30, 20),
+            height: height * 0.3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    width: (width - 60) * 0.59,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Order Item"),
+                        Expanded(
+                          child: TabView(), // Include the TabView widget here
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                // Box 2
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    width: (width - 60) * 0.39,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("data"),
+                        Expanded(
+                          child: Container(
+                            height: 50,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 50,
+                            color: Colors.deepPurple,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+// The TabView Widget
+class TabView extends StatefulWidget {
+  @override
+  _TabViewState createState() => _TabViewState();
+}
+
+class _TabViewState extends State<TabView> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(
+                    16.0,
+                  ),
+                ),
+                child: TabBar(
+                  controller: _tabController,
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      16.0,
+                    ),
+                    color: Colors.grey.shade900,
+                  ),
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.grey.shade900,
+                  tabs: [
+                    Tab(
+                      text: 'Inprogress',
+                    ),
+                    Tab(
+                      text: 'Complete',
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Inprogress Page',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'Complete Page',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

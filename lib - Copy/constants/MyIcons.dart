d6d0e7795175a49cdbeae8dcc04d9b697129cf3a
Mildwann <Mildwann46@gmail.com>
2024-06-09 01:32:27 +0700

    new almost finish but no calender

--- lib - Copy/constants/MyIcons.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Myicons {
-  static const NewIcon = Icons.difference_outlined;
-  static const ProvisioningIcon = Icons.edit_calendar_outlined;
-  static const BillingIcon = Icons.description_outlined;
-  static const CompleteIcon = Icons.description_outlined;
-}
